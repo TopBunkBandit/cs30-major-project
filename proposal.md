a9a79b2005ddb482e1b8fd997f072ba2b655062d
TopBunkBandit <jamesamitchell07@gmail.com>
2024-11-21 12:14:38 -0600

    added template for the player, added jumping and working on crouching

--- proposal.md
+++ proposal.md
@@ -1,17 +1,17 @@
 # Project Description
 
- - A 2D acrade fighting game (ie. Tekken)
+ - A 2D acrade fighting game
 
 ## Needs to have list
 
  - Basic fighting system including at minimum 2 punches (Heavy & Light), 2 kicks (Heavy & Light), a block, a crouch and a jump.
  - Multiple characters with 1 special move each
  - A health bar for each player
- - Ability to win or lose
  - vs AI or Player (Online)
 
 ## Nice to have list
 
  - Diffrent stages to play on
  - Ability to chain moves for combos (ie. a jump kick (jump and kick) or a leg sweep (crouch and kick))
+ - Ability to win or lose
  - A once per fight ultimate move(?)
--- sketch.js
+++ sketch.js
@@ -4,29 +4,89 @@
 //
 // Extra for Experts:
 // N.A.
-// TO DO LIST:
-// make a basic 'arena'
-// display the player
-// make a basic moveset class
+// CURRENT TO DO LIST:
+// change crouch function to bring player to the ground
+// have crouch be a hold to crouch and fall faster when crouching
+// 
 
 
-//contain 2 punches, 2 kicks, a jump and crouch
-class BasicMoveset{
-  constructor(){
 
+class Player{
+  constructor(x,y){
+    this.playerX = x;
+    this.playerY = y;
+    this.height = 200;
+    this.width = 50; 
+    this.gravity = 10;
+    this.airTime = 0;
+    this.currentlyCrouched = false;
+    this.lightHit = 5;
+    this.heavyHit = 15;
+    this.jumpVelocity = 5;
+  }
+  display(){
+    rect(this.playerX,this.playerY,this.width,this.height);
+  }
+  jump(){
+    this.playerY -= this.jumpVelocity;
+    this.airTime += 0.01;
+  }
+  falling(){
+    if (this.playerY <= 400 && this.playerY + this.gravity*this.airTime < 400 ){
+      this.playerY += this.gravity*this.airTime;
+    }  
+  }
+  crouchingDown(){
+    if (this.height === 200){
+      this.height = this.height/2;
+    }
+  }
+  standingUpFromCrouch(){
+    if  (this.height === 100){
+      this.height = this.height*2;
+    }
   }
 }
 
-class PlayerModels{
 
-}
-
+//john is a placeholder name, change this later
+let john;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  john = new Player(20,399);
+
 }
 
 function draw() {
   background(220);
+  rect(0,600,width,height);
+  john.display();
 
-  circle(mouseX,mouseY,100);
+  jump();
+  crouch();
+
+}
+
+
+function jump(){
+  if (keyIsDown(32)){
+    john.jump();
+  }
+  if (john.playerY + john.gravity*john.airTime < 400){
+    john.falling();
+  }
+  else{
+    john.airTime = 0;
+    john.playerY = 400;
+  }
+}
+
+
+function crouch(){
+  if (keyIsDown(17)){
+    john.crouchingDown();
+  }
+  else{
+    john.standingUpFromCrouch();
+  }
 }
