6f76bd15ec316aee5135ccd8f4fc56b9bcf0b73a
TopBunkBandit <jamesamitchell07@gmail.com>
2024-11-22 14:15:21 -0600

    added ability to properly crouch and jump, added forward movement
    also added p5.party library

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@
 //
 // Extra for Experts:
 // N.A.
-// CURRENT TO DO LIST:
-// change crouch function to bring player to the ground
-// have crouch be a hold to crouch and fall faster when crouching
-// 
+// CURRENT TO DO LIST IN ORDER OF PRIORITY:
+// add forward and backward movement
+// add 'attacks'
+// add the p5.party stuff (S.O.S.)
 
 
 
@@ -20,48 +20,76 @@ class Player{
     this.gravity = 10;
     this.airTime = 0;
     this.currentlyCrouched = false;
-    this.lightHit = 5;
-    this.heavyHit = 15;
     this.jumpVelocity = 5;
+    this.isJumping = false;
+    this.currentlyFalling = false;
+    // this.lightHit = 5;
+    // this.heavyHit = 15;
+
   }
   display(){
     rect(this.playerX,this.playerY,this.width,this.height);
   }
+
   jump(){
     this.playerY -= this.jumpVelocity;
     this.airTime += 0.01;
+    this.isJumping = true;
+    // console.log(this.airTime)
   }
+
   falling(){
     if (this.playerY <= 400 && this.playerY + this.gravity*this.airTime < 400 ){
+      if (!this.isJumping){
+        this.airTime = 0.4;
+        this.isJumping = false;
+      }
       this.playerY += this.gravity*this.airTime;
-    }  
+    }
   }
+
+  fallingWhileCrouched(){
+    if (this.playerY + this.gravity*this.airTime < 500 ){
+      if (!this.isJumping){
+        this.airTime = 0.4;
+        this.isJumping = false;
+      }
+      this.playerY += this.gravity*this.airTime*1.5;
+    }
+  }
+  
   crouchingDown(){
     if (this.height === 200){
       this.height = this.height/2;
+      this.currentlyCrouched = true;
     }
   }
+
   standingUpFromCrouch(){
     if  (this.height === 100){
       this.height = this.height*2;
+      this.currentlyCrouched = false;
     }
+  }
+
+  moveForward(){
+    this.playerX += 1;
   }
 }
 
 
 //john is a placeholder name, change this later
 let john;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   john = new Player(20,399);
-
 }
 
 function draw() {
   background(220);
   rect(0,600,width,height);
   john.display();
-
   jump();
   crouch();
 
@@ -69,18 +97,31 @@ function draw() {
 
 
 function jump(){
-  if (keyIsDown(32)){
+  if (keyIsDown(32) && john.airTime < 0.4){
     john.jump();
+    john.currentlyFalling = true;
   }
-  if (john.playerY + john.gravity*john.airTime < 400){
+  else{
+    john.isJumping = false;
+  }
+
+  if (!john.currentlyCrouched && john.playerY + john.gravity*john.airTime < 400){
     john.falling();
+  }
+  else if(!john.currentlyCrouched){
+    john.airTime = 0;
+    john.playerY = 400;
+    john.currentlyFalling = false;
+  }
+  else if(john.currentlyCrouched && john.playerY + john.gravity*john.airTime < 500 && john.currentlyFalling){
+    john.fallingWhileCrouched();
   }
   else{
     john.airTime = 0;
-    john.playerY = 400;
+    john.playerY = 500;
+    console.log(2);
   }
 }
-
 
 function crouch(){
   if (keyIsDown(17)){
@@ -88,5 +129,9 @@ function crouch(){
   }
   else{
     john.standingUpFromCrouch();
+  }
+
+  if (keyIsDown(68)){
+    john.moveForward();
   }
 }
