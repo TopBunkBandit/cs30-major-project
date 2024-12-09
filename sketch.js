bc4c3d5a74ec452bf33900afc1481d593a50c96a
TopBunkBandit <jamesamitchell07@gmail.com>
2024-12-09 14:14:13 -0600

    added a second 'player' for testing, rewrote the playerInput code so it works for both key sets
    also removed the last of the p5.party stuff, as it is not going to be implemented for a while it was only causing issues and not giving anything in return

--- sketch.js
+++ sketch.js
@@ -7,15 +7,16 @@
 // Extra for Experts:
 // N.A.
 // CURRENT TO DO LIST IN ORDER OF PRIORITY:
-// Add p5.party and have the game display two characters that can move indepentently
-// REQUIRES TESTING TO SEE IF IT WORKS
-// Add the rest of the basic abilities (block & kick)
+//add hit detection
+//add kick and block
+//fix gravity
 
 
 
 
 class Player{
   constructor(x,y){
+    //organize this sometime
     this.playerX = x;
     this.playerY = y;
     this.height = 200;
@@ -30,25 +31,33 @@ class Player{
     this.punchTime = 0;
     this.currentlyAttacking = false;
     this.facingRight = true;
-
+    this.jumpKey;
+    this.forwardKey;
+    this.backwardKey;
+    this.crouchKey;
+    this.punchKey;
   }
+
+  //displays the players
   display(){
-    shared.p1x = this.playerX;
-    shared.p1y = this.playerY;
-    shared.p1w = this.width;
-    shared.p1h = this.height;
-    rect(shared.p1x,shared.p1y,shared.p1w,shared.p1h);
-
-    //updating the shared player shape data
+    rect(this.playerX,this.playerY,this.width,this.height);
+    fill("black");
+    if (this.facingRight){
+      circle(this.playerX + this.width, this.playerY + 20, 10);     
+    }
+    else{
+      circle(this.playerX, this.playerY + 20, 10);
+    }
+    fill("white");
   }
-
+  //the code for when jump is called in playerInputs
   jump(){
     this.playerY -= this.jumpVelocity;
     this.airTime += 0.01;
     this.isJumping = true;
     // console.log(this.airTime)
   }
-
+  //the code for when falling is called in playerInputs
   falling(){
     if (this.playerY <= 400 && this.playerY + this.gravity*this.airTime < 400 ){
       if (!this.isJumping){
@@ -58,7 +67,7 @@ class Player{
       this.playerY += this.gravity*this.airTime;
     }
   }
-
+  //the code for when falling and crouching is called in playerInputs
   fallingWhileCrouched(){
     if (this.playerY + this.gravity*this.airTime < 500 ){
       if (!this.isJumping){
@@ -68,44 +77,36 @@ class Player{
       this.playerY += this.gravity*this.airTime*1.5;
     }
   }
-  
-  //attacking and locks the player into the punch
-  //need to make the rectangle draw itself backwards (to the left)
-  //maybe do this by translating the grid onto the rectangle and then rotating it by PI
+  //the code for when punch is called in playerInputs
   punch(){
+    
     if (millis() < this.oldTime + 750 && (this.playerY === 400 || this.playerY === 500)){
+      //forward punch
       if (this.facingRight){
         if (millis() < this.oldTime + 750/2){
-          console.log("a");
           rect(this.playerX + this.width/2,this.playerY + 60, this.punchTime,20);
           this.currentlyAttacking = true;
           this.punchTime += 4;
         }
         else{
-          console.log("b");
           rect(this.playerX + this.width/2,this.playerY + 60, this.punchTime,20);
           this.currentlyAttacking = true;
           this.punchTime -= 4;
         }
       }
-
-      //this is the portion that will work if your facing left
-      //almost good
+      //backward punch
       else{
-        
         push();
         translate(this.playerX,this.playerY);
         rotate(180);
         if (millis() < this.oldTime + 750/2){
-          console.log("c");
-          rect(-this.width/2, -120, this.punchTime,20);
+          rect(-this.width/2, -80, this.punchTime,20);
           this.currentlyAttacking = true;
           this.punchTime += 4;
   
         }
         else{
-          console.log("d");
-          rect(-this.width/2, -60, this.punchTime,20);
+          rect(-this.width/2, -80, this.punchTime,20);
           this.currentlyAttacking = true;
           this.punchTime -= 4;
         }
@@ -120,10 +121,27 @@ class Player{
     }
   }
 
-  playerInputs(){
-    if (!this.currentlyAttacking){
-      //crouching 
-      if (keyIsDown(17)){
+  //all current possible player inputs
+  playerInputs(player){
+    if (!this.currentlyAttacking){      
+      //IN THEORY should let the code swap what keys its looking for
+      if (player === 1){
+        this.jumpKey = 32;
+        this.forwardKey = 68;
+        this.backwardKey = 65;
+        this.crouchKey = 17;
+        this.punchKey = 81;
+      }
+      else{
+        this.jumpKey = 38;
+        this.forwardKey = 39;
+        this.backwardKey = 37;
+        this.crouchKey = 16;
+        this.punchKey = 96;
+      }
+      
+      //crouching
+      if (keyIsDown(this.crouchKey)){
         if (this.height === 200){
           this.height = this.height/2;
           this.currentlyCrouched = true;
@@ -135,25 +153,25 @@ class Player{
           this.currentlyCrouched = false;
         }
       }
-      //moving forward if not at the right wall
-      if (keyIsDown(68) && this.playerX !== width - this.width){
+      //moving forwards, should be stopping at the left wall but is not, find out why
+      if (keyIsDown(this.forwardKey) && this.playerX < width - this.width){
         this.playerX += 2;
         this.facingRight = true;
       }
       //moving backward if not at the left wall
-      if (keyIsDown(65) && this.playerX !== 0){
+      if (keyIsDown(this.backwardKey) && this.playerX !== 0){
         this.playerX -= 2;
         this.facingRight = false;
       }
       //punching
-      if (keyIsDown(81) && !currentlyHit && (this.playerY === 400 || this.playerY === 500)){
+      if (keyIsDown(this.punchKey) && !currentlyHit && (this.playerY === 400 || this.playerY === 500)){
         this.oldTime = millis();
         currentlyHit = true;
       }
 
       //this whole chunk is just jumping under diffrent conditions
       //jumping
-      if (keyIsDown(32) && this.airTime < 0.4){
+      if (keyIsDown(this.jumpKey) && this.airTime < 0.4){
         this.jump();
         this.currentlyFalling = true;
       }
@@ -179,9 +197,13 @@ class Player{
         this.airTime = 0;
         this.playerY = 500;
         console.log(2);
+
       }
+      
     }
   }
+
+
 }
 
 
@@ -191,19 +213,23 @@ currentlyHit = false;
 
 let shared;
 
-function preload() {
-  john = new Player(20,400);
-  partyConnect(
-    "wss://demoserver.p5party.org", 
-    "comp-sci30-fighting-game"
-  );
-  //need to put EVERY VALUE THAT IM GOING TO NEED TO DISPLAY TO THE OTHER PLAYER
-  shared = partyLoadShared("shared", { p1x: john.playerX, p1y: john.playerY, p1w: john.width, p1h: john.height});
-}
+// function preload() {
+
+//   partyConnect(
+//     "wss://demoserver.p5party.org", 
+//     "comp-sci30-fighting-game"
+//   );
+//   //need to put EVERY VALUE THAT IM GOING TO NEED TO DISPLAY TO THE OTHER PLAYER
+//   shared = partyLoadShared("shared", { p1x: john.playerX, p1y: john.playerY, p1w: john.width, p1h: john.height});
+// }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
+  john = new Player(50,400);
+  jim = new Player(450,400);
+  jim.facingRight = false;
+
   
 }
 
@@ -213,41 +239,19 @@ function draw() {
   rect(0,600,width,height);
   
   john.display();
-  john.playerInputs();
+  john.playerInputs(1);
   john.punch();
+
+  jim.display();
+  jim.playerInputs(2);
+  jim.punch();
 
 }
 
+//adding hit detection to the players, not player collisions
+function playerIsHit(){
+  //use john.playerY and john.punchTime to find where the arm is
+  //if it reaches jim, set a variable to true or false so it only counts the hit once
+  //vise versa for jim hitting john
 
-
-
-
-
-//p5.party demo
-
-// let shared;
-
-// function preload() {
-// 	partyConnect(
-// 		"wss://demoserver.p5party.org", 
-// 		"hello_party"
-// 	);
-//   shared = partyLoadShared("shared", { x: 100, y: 100 });
-// }
-
-// function setup() {
-//   createCanvas(400, 400);
-//   noStroke();
-// }
-
-// function mousePressed() {
-//   shared.x = mouseX;
-//   shared.y = mouseY;
-// }
-
-// function draw() {
-//   background("#ffcccc");
-//   fill("#000066");
-
-//   ellipse(shared.x, shared.y, 100, 100);
-// }
+}
